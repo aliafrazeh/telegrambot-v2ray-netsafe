e8b18b6f913c756f9e7bfa49c659251468ab9a8b
aliafrazeh <76871038+aliafrazeh@users.noreply.github.com>
2025-08-06 10:15:39 -0700

    Update user_handlers.py

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -459,10 +459,10 @@ def register_user_handlers(bot_instance, db_manager_instance, xui_api_instance):
             return
             
         configs = purchase['single_configs_json']
-        stripped = config['url'].replace("vless://", "", 1)
-        uuid = stripped.split("@")[0]
         text = messages.SINGLE_CONFIG_HEADER
         for config in configs:
+            stripped = config['url'].replace("vless://", "", 1)
+            uuid = stripped.split("@")[0]
             text += f"{uuid}"
         
         _bot.send_message(user_id, text, parse_mode='Markdown')
