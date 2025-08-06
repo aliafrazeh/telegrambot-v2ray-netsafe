45a7fb03f9f2607b372df956408d6407aaf7e075
aliafrazeh <76871038+aliafrazeh@users.noreply.github.com>
2025-08-06 09:48:48 -0700

    Update user_handlers.py

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -461,7 +461,7 @@ def register_user_handlers(bot_instance, db_manager_instance, xui_api_instance):
         configs = purchase['single_configs_json']
         text = messages.SINGLE_CONFIG_HEADER
         for config in configs:
-            text += f"**{config['remark']} ({config['protocol']}/{config['network']})**:\n`{config['url']}`\n\n  تست ربات"
+            text += f"**{config['remark']} {config['network']}`\n\n"
         
         _bot.send_message(user_id, text, parse_mode='Markdown')
         
